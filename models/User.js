e0ff67c1ab44617a3e2f5149c2d4917e4445e531
Gaurav Bhatt <gbhatt570@gmail.com>
2025-08-07 18:09:16 +0530

    fix: correct import statement for Notification model to include file extension

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import Notification from './Notification';
+import Notification from './Notification.js';
 
 const userSchema = new mongoose.Schema({
   username: {
