dc6dab85c1c38d25cad1b66f921f79ecfb58554c
Gaurav Bhatt <gbhatt570@gmail.com>
2025-10-06 01:20:24 +0530

    chore: update Dockerfile to use Node.js 22 and simplify MongoDB connection options

--- config/db.js
+++ config/db.js
@@ -1,15 +1,11 @@
 // config/db.js
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
-dotenv.config();
+dotenv.config()
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI); // options not needed
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
